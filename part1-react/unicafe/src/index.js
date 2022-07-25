c2c63d0e2f62ce2e4e122a32025eb339bfb6f16d
Arcangel Maldonado <nemaldonado663@gmail.com>
2022-07-25 00:45:58 -0300

    Unicafe step-2

--- part1-react/unicafe/src/index.js
+++ part1-react/unicafe/src/index.js
@@ -1,25 +1,60 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-const Statistics = (props) => {
-  // ...
+const App = () => {
+
+  // save clicks of each button to its own state
+  // const [good, setGood] = useState(0)
+  // const [neutral, setNeutral] = useState(0)
+  // const [bad, setBad] = useState(0)
+
+
+
+  const[counters, setCounters] = useState({
+    good : 0,
+    neutral: 0,
+    bad: 0,
+  })
+  const All = () => {
+    const allClicks = counters.good + counters.neutral + counters.bad
+    return allClicks
+  }
+
+  const Average = (promps) =>{
+    const averageClicks = All()
+    const good = counters.good
+    const bad = counters.bad
+    const Avg = (good - bad) / averageClicks
+    return <strong>average {Avg}</strong>
 }
 
-const App = () => {
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+const Positive = () =>{
+  const persentil = counters.good / All()*100
+  return <strong>positive {persentil}%</strong>
+}
 
-  const goodClick = () => {
-    setGood(good + 1)
+
+const goodClick = () => {
+  const newCounterState = {
+    ...counters,
+    good: counters.good + 1,
   }
-  const neutralClick = () => {
-    setNeutral(neutral + 1)
+  setCounters(newCounterState);
+}
+const neutralClick = () => {
+  const newCounterState = {
+    ...counters,
+    neutral: counters.neutral + 1,
   }
-  const badClick = () => {
-    setBad(bad + 1)
+  setCounters(newCounterState);
+}
+const badClick = () => {
+  const newCounterState = {
+    ...counters,
+    bad: counters.bad + 1,
   }
+  setCounters(newCounterState);
+}
   
   return (
     <div>
@@ -28,9 +63,13 @@ const App = () => {
       <button onClick={neutralClick}>neutral</button>
       <button onClick={badClick}>bad</button>
       <h2>statistics</h2>
-      <strong>good {good}</strong><br />
-      <strong>neutral {neutral}</strong><br />
-      <strong>bad {bad}</strong>
+      <strong>good {counters.good}</strong><br />
+      <strong>neutral {counters.neutral}</strong><br />
+      <strong>bad {counters.bad}</strong><br />
+      <strong>all <All /></strong><br />
+      <Average all={All}/><br />
+      <Positive />
+
     </div>
   )
 }
