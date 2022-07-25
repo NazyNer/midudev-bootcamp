22e9f9c06dd847a9f510e51132106db36891c74b
Arcangel Maldonado <nemaldonado663@gmail.com>
2022-07-25 12:12:37 -0300

    unicafe step-6

--- part1-react/unicafe/src/index.js
+++ part1-react/unicafe/src/index.js
@@ -1,76 +1,109 @@
+
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-const App = () => {
-
-  // save clicks of each button to its own state
-  // const [good, setGood] = useState(0)
-  // const [neutral, setNeutral] = useState(0)
-  // const [bad, setBad] = useState(0)
-
-
-
-  const[counters, setCounters] = useState({
-    good : 0,
-    neutral: 0,
-    bad: 0,
-  })
-  const All = () => {
-    const allClicks = counters.good + counters.neutral + counters.bad
-    return allClicks
+const Positive = ({good, neutral, bad}) =>{
+  const All = good + neutral + bad
+  const persentil = good / All*100
+  if (isNaN(persentil)){
+    return <tr><td>positive</td><td>0%</td></tr>
   }
-
-  const Average = (promps) =>{
-    const averageClicks = All()
-    const good = counters.good
-    const bad = counters.bad
-    const Avg = (good - bad) / averageClicks
-    return <strong>average {Avg}</strong>
+  return <tr><td>positive</td><td>{persentil}%</td></tr>
 }
 
-const Positive = () =>{
-  const persentil = counters.good / All()*100
-  return <strong>positive {persentil}%</strong>
-}
-
-
-const goodClick = () => {
-  const newCounterState = {
-    ...counters,
-    good: counters.good + 1,
+const Average = ({goods, neutrals, bads}) => {
+  const averageClicks = goods + neutrals + bads
+  const good = goods
+  const bad = bads
+  const Avg = (good - bad) / averageClicks
+  if (isNaN(Avg)){
+    return <tr><td>average</td><td>0%</td></tr>
   }
-  setCounters(newCounterState);
+  return <tr><td>average</td><td>{Avg}</td></tr>
 }
-const neutralClick = () => {
-  const newCounterState = {
-    ...counters,
-    neutral: counters.neutral + 1,
-  }
-  setCounters(newCounterState);
-}
-const badClick = () => {
-  const newCounterState = {
-    ...counters,
-    bad: counters.bad + 1,
-  }
-  setCounters(newCounterState);
-}
-  
+
+const Button = ({clicksGood, clicksNeutral, clicksBad}) => {
   return (
     <div>
-      <h1>give feedback</h1>
-      <button onClick={goodClick}>good</button>
-      <button onClick={neutralClick}>neutral</button>
-      <button onClick={badClick}>bad</button>
-      <h2>statistics</h2>
-      <strong>good {counters.good}</strong><br />
-      <strong>neutral {counters.neutral}</strong><br />
-      <strong>bad {counters.bad}</strong><br />
-      <strong>all <All /></strong><br />
-      <Average all={All}/><br />
-      <Positive />
-
+        <h1>give feedback</h1>
+        <button onClick={clicksGood}>good</button>
+        <button onClick={clicksNeutral}>neutral</button>
+        <button onClick={clicksBad}>bad</button>
     </div>
+  )
+}
+const Statistic = ({text, value}) =>{
+  return (
+    <tr>
+      <td>{text}</td><td>{value}</td>  
+    </tr>
+  )
+}
+
+const Statistics = ({good, neutral, bad}) => {
+  return(
+    <>
+      <Statistic text="good" value ={good} />
+      <Statistic text="neutral" value ={neutral} />
+      <Statistic text="bad" value ={bad} />
+    </>
+  )
+}
+
+const All = ({good, neutral, bad}) => {
+  const allClicks = good + neutral + bad
+  return allClicks
+}
+
+const Table = ({good, neutral, bad}) => {
+  if(All({good, neutral, bad}) === 0){
+    return (
+      <div>
+        <h2>No feedback given</h2>
+      </div>
+    )
+  }
+  return (
+    <div>
+      <h2>Statistics</h2>
+      <table>
+        <Statistics good={good} neutral={neutral} bad={bad} />
+        <tr><td>all</td><td><All good={good} neutral={neutral} bad={bad}/></td></tr>
+        <Average goods={good} neutrals={neutral} bads={bad} />
+        <Positive good={good} neutral={neutral} bad={bad} />
+      </table>
+  </div>
+  )
+}
+
+const App = (props) => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+
+const clicksGood = () => {
+  setGood((prevGood) => {
+    return prevGood + 1
+  })
+}
+const clicksNeutral = () => {
+  setNeutral((prevNeutral) => {
+    return prevNeutral + 1
+  })
+}
+const clicksBad = () => {
+  setBad((prevBad) => {
+    return prevBad + 1
+  })
+}
+
+  return (
+      <div>
+        <Button clicksGood={clicksGood} clicksNeutral={clicksNeutral} clicksBad={clicksBad}/>
+        <Table good={good} neutral={neutral} bad={bad} />
+      </div>
   )
 }
 
